9124cd3cd2910faff250a48a4b05143a3c9bc272
Sergio Torres <sergio.torres@mendeley.com>
2014-09-19 11:25:50 +0100

    Added wallpaper tutorial from vogella

---
+++ app/src/main/java/com/garrapeta/gameoflive/MyPoint.java
@@ -0,0 +1,14 @@
+package com.garrapeta.gameoflive;
+
+
+public class MyPoint {
+    String text;
+    final int x;
+    final int y;
+
+    public MyPoint(String text, int x, int y) {
+        this.text = text;
+        this.x = x;
+        this.y = y;
+    }
+}
